d4f6af2b80770b5101f964b3d2e675a3f6b28879
blake <blake@google.com>
2023-08-22 21:47:19 +0800

    Init or reset first admin user

--- app/Console/Commands/Password.php
+++ app/Console/Commands/Password.php
@@ -43,25 +43,26 @@ class Password extends Command
         $r = AdminRole::query()->first();
         if($r == false){
             $r = new AdminRole();
+            $r->id = 1;
             $r->name = $r->identity = 'boss';
             $r->status = 1;
             $r->create_time = $r->update_time = time();
-            $r->permission = implode(',',array_values(array_column( AdminPermission::query()->select('id')->get()->toArray(),'id')));
+            $r->permission = implode('|',array_values(array_column( AdminPermission::query()->select('id')->get()->toArray(),'id')));
             $r->save();
         }
 
         $m           = \App\Models\Admin::query()->first();
         if($m == false){
             $m = new Admin();
-            $m->admin_role_id = $r->save();
-            $m->name = 'boss';
-            $m->mobile = '110';
-            $m->username = 'boss';
-            $m->status = 1;
-            $m->remember_token = '';
-            $m->google_authenticator = '';
-            $m->create_time = time();
         }
+        $m->admin_role_id = 1;
+        $m->name = 'boss';
+        $m->mobile = '110';
+        $m->username = 'boss';
+        $m->status = 1;
+        $m->remember_token = '';
+        $m->google_authenticator = '';
+        $m->create_time = time();
         $m->password = bcrypt('123456'.config('config.secret_key'));
         $m->update_time = time();
         $m->save();
